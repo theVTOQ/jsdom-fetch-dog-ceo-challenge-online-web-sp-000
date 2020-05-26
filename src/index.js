014cfc13ba1a795d993c5172588d696129f5d6a0
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-05-26 05:15:41 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 console.log('%c HI', 'color: firebrick')
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+let breeds = [];
+let filteredBreeds = [];
 
 window.addEventListener('DOMContentLoaded', (event) => {
-  fetch(imgUrl).then(response => response.json()).then(json => addJSONImagesToDom(json));
+  //fetch(imgUrl).then(response => response.json()).then(json => addJSONImagesToDom(json));
+
+  fetch(breedUrl).then(response => response.json()).then(json => addJSONBreedsToDom(json));
 });
 
 function addJSONImagesToDom(json){
@@ -11,11 +16,75 @@ function addJSONImagesToDom(json){
   const imageList = document.createElement("ul");
   imageListDiv.appendChild(imageList);
 
-  //console.log(json.message[0]);
-  for (let i = 0; i < json.message.length; i++){
+  for (const element of json.message){
     const newImage = document.createElement("img");
     //imageList.innerHtml += `<li><img src="${json.message[i]}"></li>`;
-    newImage.src = json.message[i];
+    //newImage.src = json.message[i];
+    newImage.src = element;
     imageList.appendChild(newImage);
   }
+}
+
+function addJSONBreedsToDom(json){
+  breeds = Object.keys(json.message);
+  //filteredBreeds = breeds;
+  renderBreeds(breeds);
+
+  const breedNameFilter = document.getElementById("breed-dropdown");
+  //breedNameFilter.addEventListener("change", filterBreeds());
+  breedNameFilter.addEventListener("change", function(){
+    clearBreedList();
+    const filterLetter = breedNameFilter.value;
+    renderBreeds(filterBreeds(filterLetter));
+  });
+}
+
+function renderBreeds(breedList) {
+  clearBreedList();
+  const breedUl = document.getElementById("dog-breeds");
+
+  for(const breed of breedList){
+    // for(breed of breedList){
+    //   const li = document.createElement("li");
+    //   li.innerHtml = breed;
+    //   breedUl.appendChild(li);
+    // }
+    const li = document.createElement("li");
+    li.innerText = breed;
+    breedUl.appendChild(li);
+  }
+
+  addColorChangingEventListenersToBreeds();
+}
+
+function addColorChangingEventListenersToBreeds(){
+  const breedLis = document.querySelectorAll("#dog-breeds li");
+
+  breedLis.forEach(breedLi => {
+    breedLi.addEventListener("click", function(){
+      breedLi.style.color = "red";
+    });
+  });
+}
+
+function clearBreedList(){
+  const breedUl = document.getElementById("dog-breeds");
+  breedUl.textContent = "";
+}
+
+function filterBreeds(filterLetter) {
+  const filterBreeds = [];
+
+  for(const breed of breeds){
+    const startingLetterOfBreed = breed[0]; //first character of string
+    if (startingLetterOfBreed == filterLetter){
+      filteredBreeds.push(breed);
+    }else if (startingLetterOfBreed > filterLetter) {
+      //breeds is an array of breed names in alphabetical order;
+      //therefore, if the starting letter of an element in breeds is
+      //alphabetically greater than startingLetter, no need to proceed
+      return filteredBreeds;
+    }
+  }
+  return filteredBreeds;
 }
